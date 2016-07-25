12b0c8ea3aaf9033046dcee2ac88a0f327eb5feb
memolition <memolition@outlook.com>
2016-07-24 21:18:36 -0600

    Commented JS file

--- js/carousel.js
+++ js/carousel.js
@@ -2,9 +2,9 @@ var carousel = document.getElementById('carousel');
 var width = 600;
 var initial = 0;
 var carouselInterval = setInterval(function() {
-    if ((carousel.scrollWidth - 50) >= (carousel.scrollLeft + width)) {
+    if ((carousel.scrollWidth - 50) >= (carousel.scrollLeft + width)) { //If didn't reached final image 
         //Move to next image
-        initial = carousel.scrollLeft;
+        initial = carousel.scrollLeft; //Set initial scrollLeft to calculate final image position
 
         function animateScroll() {
             var time = 1; //Seconds on animation to switch
@@ -12,25 +12,24 @@ var carouselInterval = setInterval(function() {
 
             carousel.scrollLeft += frame; //Scroll 
             if (carousel.scrollLeft < initial + width) { //If image isn't fixed within carousel viewport
-                window.requestAnimationFrame(animateScroll);
+                window.requestAnimationFrame(animateScroll); //Call next frame
             }
         }
-        window.requestAnimationFrame(animateScroll);
-    } else {
-        //Reset position
-        initial = 0;
-
+        window.requestAnimationFrame(animateScroll); //Start animation
+    } else { //If reached last image on carousel
+        //Reset carousel position
+        initial = 0; //Set initial position to calculate final carousel position
         function animateReset() {
             var time = 1; //Seconds on animation to switch
             var frame = carousel.scrollLeft / 30; //How many pixels to move
 
             carousel.scrollLeft -= frame; //Scroll 
             if (carousel.scrollLeft > 15) { //If image isn't fixed within carousel viewport
-                window.requestAnimationFrame(animateReset);
+                window.requestAnimationFrame(animateReset); //Call next frame
             } else {
-                carousel.scrollLeft = 0;
+                carousel.scrollLeft = 0; //Fallback to 0 even if time is up.
             }
         }
-        window.requestAnimationFrame(animateReset);
+        window.requestAnimationFrame(animateReset); //Start animation to reset carousel
     }
 }, 4000)
