86d4db3bc2e634010bdd8d1303d586c9605f960b
memolition <memolition@outlook.com>
2016-07-24 21:06:21 -0600

    Animation completed

--- js/carousel.js
+++ js/carousel.js
@@ -1,8 +1,36 @@
 var carousel = document.getElementById('carousel');
+var width = 600;
+var initial = 0;
 var carouselInterval = setInterval(function() {
-    if ((carousel.scrollWidth - 50) >= (carousel.scrollLeft + 600)) {
-        carousel.scrollLeft += 605;
+    if ((carousel.scrollWidth - 50) >= (carousel.scrollLeft + width)) {
+        //Move to next image
+        initial = carousel.scrollLeft;
+
+        function animateScroll() {
+            var time = 1; //Seconds on animation to switch
+            var frame = width / (60 * time); //How many pixels to move
+
+            carousel.scrollLeft += frame; //Scroll 
+            if (carousel.scrollLeft < initial + width) { //If image isn't fixed within carousel viewport
+                window.requestAnimationFrame(animateScroll);
+            }
+        }
+        window.requestAnimationFrame(animateScroll);
     } else {
-        carousel.scrollLeft = 0;
+        //Reset position
+        initial = 0;
+
+        function animateReset() {
+            var time = 1; //Seconds on animation to switch
+            var frame = carousel.scrollLeft / 30; //How many pixels to move
+
+            carousel.scrollLeft -= frame; //Scroll 
+            if (carousel.scrollLeft > 15) { //If image isn't fixed within carousel viewport
+                window.requestAnimationFrame(animateReset);
+            } else {
+                carousel.scrollLeft = 0;
+            }
+        }
+        window.requestAnimationFrame(animateReset);
     }
-}, 3000);
+}, 4000)
